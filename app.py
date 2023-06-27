96b3d1815ccffee21dd2091c8dc98c1de13bcc7e
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-06-27 14:20:14 +0700

    fix COR error

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from Transformer.model import Transformer
 
 app = Flask(__name__)
+CORS(app)
 
 model = Transformer()
 model.load_model()
