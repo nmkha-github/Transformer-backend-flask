dcc6719253aa2a48f683d0597fd7a5617fc4db48
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-06-28 23:15:15 +0700

    update question-answering api

--- Transformer/model.py
+++ Transformer/model.py
@@ -2,20 +2,26 @@ import torch
 from transformers import pipeline
 
 
-class Transformer:
+class Model:
     def __init__(self):
         self.summarizer = None
+        self.question_answering = None
 
-    def load_model(self):
+    def load_model(self, task):
         try:
-            self.summarizer = pipeline(
-                'summarization', 'phongle1311/my_awesome_billsum_model')
-            # self.summarizer = torch.load('Transformer/m.pkl')
+            if task == 'summarization':
+                self.summarizer = pipeline(
+                    'summarization', 'phongle1311/my_awesome_billsum_model')
+            elif task == 'question-answering':
+                self.question_answering = pipeline("question-answering")
         except:
             print('$$$$$$$$$$$$$$$$$$ load model error ')
             return False
 
         return True
 
-    def summarize(self, text):
+    def summarization(self, text):
         return self.summarizer(text)
+
+    def question_answering(self, context, question):
+        return self.question_answering(context=context, question=question)
--- app.py
+++ app.py
@@ -1,19 +1,53 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-from Transformer.model import Transformer
+import json
+from Transformer.Model import Model
 
 app = Flask(__name__)
 CORS(app)
 
-model = Transformer()
-model.load_model()
+model = Model()
+
+model.load_model('question-answering')
 
 
-@app.route('/api/summarize', methods=['POST', 'GET'])
-def summarize():
+@app.route('/api/question_answering', methods=['POST', 'GET'])
+def question_answering():
     global model
-    print('$$$$$$$$$$$$$$$$$$$$$$$$', model)
+    if request.method == 'POST':
+        context = ''
+        question = ''
+        try:
+            data = request.get_json()
+            context = data['context']
+            print(context)
+            question = data['question']
+            print(question)
+        except:
+            return {'status': "Data must have content field"}
+
+        answer_json = model.question_answering(
+            context=context, question=question)
+        return answer_json
+    else:
+        context_sample = 'A is 5 years old and lives with her mother in Ha Noi city'
+        question_sample = 'where does A live?'
+        result = model.question_answering(
+            context=context_sample, question=question_sample)
+        return "<p><strong>context:</strong> " + context_sample \
+            + "<br><strong>question:</strong> " + question_sample \
+            + "<br><strong>answer:</strong> " + result['answer'] \
+            + "<br>" + json.dumps(result) + \
+            "</p>"
+
+
+model.load_model('summarization')
+
+
+@app.route('/api/summarization', methods=['POST', 'GET'])
+def summarization():
+    global model
     if request.method == 'POST':
         text = ""
         try:
@@ -22,10 +56,10 @@ def summarize():
         except:
             return {'status': "Data must have content field"}
 
-        summary_text = model.summarize(text)[0]
+        summary_text = model.summarization(text)[0]
         return summary_text
     else:
-        summary_text = model.summarize(
+        summary_text = model.summarization(
             "National Archives Yes, it’s that time again, folks. It’s the first Friday of the month, when for one ever-so-brief moment the interests of Wall Street, Washington and Main Street are all aligned on one thing: Jobs. A fresh update on the U.S. employment situation for January hits the wires at 8:30 a.m. New York time offering one of the most important snapshots on how the economy fared during the previous month. Expectations are for 203,000 new jobs to be created, according to economists polled by Dow Jones Newswires, compared to 227,000 jobs added in February. The unemployment rate is expected to hold steady at 8.3%. Here at MarketBeat HQ, we’ll be offering color commentary before and after the data crosses the wires. Feel free to weigh-in yourself, via the comments section. And while you’re here, why don’t you sign up to follow us on Twitter. Enjoy the show. ||||| Employers pulled back sharply on hiring last month, a reminder that the U.S. economy may not be growing fast enough to sustain robust job growth. The unemployment rate dipped, but mostly because more Americans stopped looking for work. The Labor Department says the economy added 120,000 jobs in March, down from more than 200,000 in each of the previous three months. The unemployment rate fell to 8.2 percent, the lowest since January 2009. The rate dropped because fewer people searched for jobs. The official unemployment tally only includes those seeking work. The economy has added 858,000 jobs since December _ the best four months of hiring in two years. But Federal Reserve Chairman Ben Bernanke has cautioned that the current hiring pace is unlikely to continue without more consumer spending.")
         print(summary_text)
         return summary_text
