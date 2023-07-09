67e6c639a787507bca6c128216aa0ccf3448894e
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-07-09 10:28:23 +0700

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 
 import json
-from Transformer.Model import Model
+from Transformer import Model
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
